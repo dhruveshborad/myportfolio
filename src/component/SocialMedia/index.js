8596be049470381bc749d0d9c6e4f79e0bed5700
Dhruvesh <dhruveshborad007@gmail.com>
2023-12-11 16:04:47 +0530

    create page

--- src/component/SocialMedia/index.js
+++ src/component/SocialMedia/index.js
@@ -1,44 +1,47 @@
+import Image from "next/image";
 import Link from "next/link";
+import Twitter from "../../../public/assets/twitter.svg";
+import Facebook from "../../../public/assets/facebook.svg";
+import Instagram from "../../../public/assets/instagram.svg";
+import Linkedin from "../../../public/assets/linkedin.svg";
 
 const SocialMedia = () => {
   return (
     <div className="social-media">
-      <div class="social-links">
-        <ul>
-          <li>
-            <Link
-              href="https://x.com/BoradDhruvesh?t=wRcjUXcjXD8gijdH-9hnug&s=08"
-              class="twitter"
-              target="_blank"
-            >
-              <i class="bi bi-twitter">twitter</i>
-            </Link>
-          </li>
-          <li>
-            <Link href="#" class="facebook">
-              <i class="bi bi-facebook">facebook</i>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="https://www.instagram.com/d_j_borad/"
-              class="instagram"
-              target="_blank"
-            >
-              <i class="bi bi-instagram">instagram</i>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="https://www.linkedin.com/in/dhruveshkumar-borad-680505214/"
-              class="linkedin"
-              target="_blank"
-            >
-              <i class="bi bi-linkedin">linkedin</i>
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <ul class="socialLinks">
+        <li>
+          <Link
+            href="https://x.com/BoradDhruvesh?t=wRcjUXcjXD8gijdH-9hnug&s=08"
+            class="twitter"
+            target="_blank"
+          >
+            <Image src={Twitter} alt="twitter" />
+          </Link>
+        </li>
+        <li>
+          <Link href="#" class="facebook">
+            <Image src={Facebook} alt="facebook" />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="https://www.instagram.com/d_j_borad/"
+            class="instagram"
+            target="_blank"
+          >
+            <Image src={Instagram} alt="instagram" />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="https://www.linkedin.com/in/dhruveshkumar-borad-680505214/"
+            class="linkedin"
+            target="_blank"
+          >
+            <Image src={Linkedin} alt="linkedin" />
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,27 @@
 import HeaderNavbar from "@/component/HeaderNavbar";
 import SocialMedia from "@/component/SocialMedia";
+import Me from "../../public/assets/me.png";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <div>
+    <div className="container">
+      <div className="titleDes">
         <h1>DHRUVESH BORAD</h1>
-        <p>
-          Experienced Full-stack Developer with a focus on React.js, skilled in
-          building scalable web applications with clean, maintainable code.
-          Proficient in both front-end development using React and back-end
-          technologies like Node.js, dedicated to delivering seamless user
-          experiences and staying abreast of the latest React.js advancements.
-        </p>
+        <h3>
+          Experienced Full-stack Developer focused on React.js, skilled in
+          building scalable web applications with clean, maintainable code,
+          proficient in both front-end (React) and back-end (Node.js)
+          development for seamless user experiences.
+        </h3>
         <div>
           <HeaderNavbar />
           <SocialMedia />
         </div>
       </div>
-    </>
+      <div className="MyPhoto">
+        <Image src={Me} alt="image" />
+      </div>
+    </div>
   );
 }
